b416b29e4588a7492d419c9d5a6581bee8732be1
AyazAktaş <ayazaktastr@gmail.com>
2024-08-18 16:29:49 +0300

    kişi detay ve kişi kayıt data binding eklendi ve kodları düzeltildi

--- app/src/main/java/com/example/contactsapplicationv1/ui/fragment/KisiDetayFragment.kt
+++ app/src/main/java/com/example/contactsapplicationv1/ui/fragment/KisiDetayFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.navArgs
 import com.example.contactsapplicationv1.R
 import com.example.contactsapplicationv1.databinding.FragmentKisiDetayBinding
@@ -14,24 +15,19 @@ import com.example.contactsapplicationv1.databinding.FragmentKisiDetayBinding
 class KisiDetayFragment : Fragment() {
     private lateinit var binding: FragmentKisiDetayBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding=FragmentKisiDetayBinding.inflate(inflater, container, false)
-
-        binding.toolbarKisiDetay.title="Kişi Detay Güncelleme Sayfası"
+        binding=DataBindingUtil.inflate(inflater,R.layout.fragment_kisi_detay, container, false)
+        binding.kisiDetayFragment=this
+        binding.kisiDetayToolbarBaslik="Kişi Detay Güncelleme Sayfası"
 
         val bundle:KisiDetayFragmentArgs by navArgs()
         val gelenKisi=bundle.kisi
-
+        binding.kisiNesnesi=gelenKisi
         binding.editTextKisiAdGuncelle.setText(gelenKisi.kisi_Ad)
         binding.editTextKisiTelGuncelle.setText(gelenKisi.kisi_Tel)
-        binding.buttonGuncelle.setOnClickListener {
-            val kisi_Ad=binding.editTextKisiAdGuncelle.text.toString()
-            val kisi_Tel=binding.editTextKisiTelGuncelle.text.toString()
-            guncelle(gelenKisi.kisi_id,kisi_Ad,kisi_Tel)
-        }
 
         return binding.root
     }
-    fun guncelle(kisi_Id:Int,kisi_Ad:String,kisi_Tel:String){
+    fun buttonGuncelle(kisi_Id:Int,kisi_Ad:String,kisi_Tel:String){
         Log.e("Kisi Guncelle","$kisi_Id - $kisi_Ad - $kisi_Tel")
     }
 }
--- app/src/main/java/com/example/contactsapplicationv1/ui/fragment/KisiKayitFragment.kt
+++ app/src/main/java/com/example/contactsapplicationv1/ui/fragment/KisiKayitFragment.kt
@@ -6,25 +6,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.contactsapplicationv1.R
 import com.example.contactsapplicationv1.databinding.FragmentKisiKayitBinding
 
 class KisiKayitFragment : Fragment() {
     private lateinit var binding: FragmentKisiKayitBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding=FragmentKisiKayitBinding.inflate(inflater, container, false)
-
-        binding.toolbarKisiKayit.title="Kişi Kayıt Ekranı"
-        binding.buttonKaydet.setOnClickListener {
-            val kisi_Ad=binding.editTextKisiAd.text.toString()
-            val kisi_Tel=binding.editTextKisiTel.text.toString()
-            kaydet(kisi_Ad,kisi_Tel)
-        }
+        binding=DataBindingUtil.inflate(inflater, R.layout.fragment_kisi_kayit,container, false)
+        binding.kisiKayitFragment=this
+        binding.kisiKayitToolbarBaslik="Kişi Kayıt Ekranı"
 
         return binding.root
     }
 
-    fun kaydet(kisi_Ad:String,kisi_Tel:String){
+    fun buttonKaydet(kisi_Ad:String,kisi_Tel:String){
         Log.e("Kisi Kaydet","$kisi_Ad - $kisi_Tel")
     }
 
