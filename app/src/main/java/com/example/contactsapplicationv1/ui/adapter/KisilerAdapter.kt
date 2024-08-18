202830038c36b76638d7622dfe18a7086f068bda
AyazAktaş <ayazaktastr@gmail.com>
2024-08-18 16:45:04 +0300

    card view üzerinde

--- app/src/main/java/com/example/contactsapplicationv1/ui/adapter/KisilerAdapter.kt
+++ app/src/main/java/com/example/contactsapplicationv1/ui/adapter/KisilerAdapter.kt
@@ -5,8 +5,10 @@ import android.icu.text.Transliterator.Position
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
+import com.example.contactsapplicationv1.R
 import com.example.contactsapplicationv1.data.entity.Kisiler
 import com.example.contactsapplicationv1.databinding.CardTasarimBinding
 import com.example.contactsapplicationv1.databinding.FragmentAnasayfaBinding
@@ -19,7 +21,7 @@ class KisilerAdapter(var mContext:Context,var kisilerListesi:List<Kisiler>):  //
     inner class CardTasarimHolder(var tasarim:CardTasarimBinding):RecyclerView.ViewHolder(tasarim.root)//hpşder binding işlem yapılmasıın sağlar
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardTasarimHolder {
-        val binding=CardTasarimBinding.inflate(LayoutInflater.from(mContext),parent,false )
+        val binding:CardTasarimBinding=DataBindingUtil.inflate(LayoutInflater.from(mContext),R.layout.card_tasarim,parent,false )
         return CardTasarimHolder(binding) //bniding kurulumu
     }
 
@@ -28,8 +30,7 @@ class KisilerAdapter(var mContext:Context,var kisilerListesi:List<Kisiler>):  //
         val kisi=kisilerListesi.get(position)
         val t = holder.tasarim
         // t card_tasarim.xml deki nesnelere erişmeni sağlar
-        t.textViewKisiAd.text=kisi.kisi_Ad
-        t.textViewKisiTel.text=kisi.kisi_Tel
+        t.kisiNesnesi=kisi
 
         t.cardViewSatir.setOnClickListener {
             val gecis=AnasayfaFragmentDirections.kisiDetayGecis(kisi=kisi)
